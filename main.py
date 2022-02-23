5574bff3a6c490c17f17fd6a02dfd76fbcffa0e5
Ben Belland <benbelland@gmail.com>
2022-02-22 21:24:00 -0800

    working algo

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import itertools
+
 
 class Rotation(object):
     #qwer asdf
@@ -33,29 +35,69 @@ class Rotation(object):
         'd' : False,
         'f' : False
         }
-    queue = list()
+    queue = list(itertools.permutations(['q', 'w', 'e', 'r', 'a', 's', 'd', 'f']))
+    #print(queue)
+    total = 0
+    compare = 0
     
     def doInput(self):
         print("Setting rotation skills...")
         self.keys = input("Which keys should we set? (qwerasdf)")
-        biggest = 0
+        self.biggest = 0
         for key in self.keys:
-            self.cooldowns[key] = input("Skill "+key+" cooldown?: ")
+            self.cooldowns[key] = int(input("Skill "+key+" cooldown?: "))
             self.queue.append(self.cooldowns[key])
-            if self.cooldowns[key] > biggest:
-                biggest = self.cooldowns[key]
+            if self.cooldowns[key] > self.biggest:
+                self.biggest = self.cooldowns[key]
         #print(self.queue[0])
-        self.fuzziness = input("Fuzziness? (leeway) :")
+        self.coolQueue = list(itertools.permutations(self.cooldowns))
+        #print(self.coolQueue)
+        self.fuzziness = int(input("Fuzziness? (leeway) :"))
+    def findOut(self):
+        for i in self.coolQueue:
+            tot = 0
+            count = 0
+            first = 0
+            for x in i:
+                dontloop = False
+                if first == 0:
+                    first = self.cooldowns[x]
+                    dontloop = True
+                if dontloop == False:
+                    tot = self.cooldowns[x] + tot
+                if tot > first and tot > first + self.fuzziness:
+                    return False
+                elif tot < first:
+                    print("Total less than first: "+ str(tot) + ":" +str(first))
+                elif tot == first and dontloop == False:
+                    return i[:count+1], count
+                count += 1
+    def recurse(self):
         
+        for key in self.keys:
+            if self.added[key] == False:
+                self.added[key] = True
+                self.total = self.total + self.cooldowns[key]
+                self.skills[key] = key
+                print(key)
+                #self.queue.append(self.skills[i])
+                if self.biggest < self.total:
+                     return "biggest reached"
+                else:
+                     self.recurse()
+        #otherthing = thing + recurse(thing)
+        
+        #return recurse(otherthing)
     def addCooldown(self, one, two):
         three = one + two
         return three
     def calculateRotation(self):
         print("Placeholder")
-        count = 0
-        for key in self.keys:
-            self.addCooldown(self.queue[count], self.queue[count]) 
+        #for key in self.keys:
+        #    self.addCooldown(self.queue[], self.queue[count]) 
 r = Rotation()
 
 r.doInput()
-print(r.addCooldown(1, 2))
+#print(r.addCooldown(1, 2))
+print(r.findOut())
+#print(r.recurse())
